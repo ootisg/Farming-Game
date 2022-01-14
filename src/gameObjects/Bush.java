5edf9cc99bb5aa078c8c5698723479e13fb08525
ootisg <ootisg@illcomics.com>
2022-01-14 00:20:52 -0600

    Bushes now sometimes drop seeds

--- src/gameObjects/Bush.java
+++ src/gameObjects/Bush.java
@@ -3,7 +3,15 @@ package gameObjects;
 import java.util.Random;
 
 import gui.Interactable;
+import items.BeetSeeds;
+import items.CarrotSeeds;
+import items.CornSeeds;
 import items.Hay;
+import items.PotatoSeeds;
+import items.PumpkinSeeds;
+import items.StrawberrySeeds;
+import items.TomatoSeeds;
+import items.WatermelonSeeds;
 import main.GameObject;
 
 public class Bush extends Debris implements Interactable {
@@ -41,6 +49,52 @@ public class Bush extends Debris implements Interactable {
 	public void click() {
 		onHarvest ();
 		getGui ().getInventory ().addItem (new Hay ());
+		if (Math.random () < .1) {
+			//Drop rolled for seeds (10%)
+			int season = (int)(Math.random () * 3); //TODO swap based on actual season
+			if (season == 0) {
+				//Spring seeds
+				int rollVal = (int)(Math.random () * 3);
+				switch (rollVal) {
+					case 0:
+						getGui ().getInventory ().addItem (new CarrotSeeds ());
+						break;
+					case 1:
+						getGui ().getInventory ().addItem (new PotatoSeeds ());
+						break;
+					case 2:
+						getGui ().getInventory ().addItem (new StrawberrySeeds ());
+						break;
+				}
+			}
+			if (season == 1) {
+				//Summer seeds
+				int rollVal = (int)(Math.random () * 3);
+				switch (rollVal) {
+					case 0:
+						getGui ().getInventory ().addItem (new TomatoSeeds ());
+						break;
+					case 1:
+						getGui ().getInventory ().addItem (new WatermelonSeeds ());
+						break;
+					case 2:
+						getGui ().getInventory ().addItem (new CornSeeds ());
+						break;
+				}
+			}
+			if (season == 2) {
+				//Fall seeds
+				int rollVal = (int)(Math.random () * 2);
+				switch (rollVal) {
+					case 0:
+						getGui ().getInventory ().addItem (new BeetSeeds ());
+						break;
+					case 1:
+						getGui ().getInventory ().addItem (new PumpkinSeeds ());
+						break;
+				}
+			}
+		}
 		forget ();
 	}
 
