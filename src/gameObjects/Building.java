4032e2129d9df04a7536627b4dfc77ee9b16bbd9
ootisg <ootisg@illcomics.com>
2022-01-20 23:31:23 -0600

    Tried to add something and it broke stuff

--- src/gameObjects/Building.java
+++ src/gameObjects/Building.java
@@ -1,18 +1,43 @@
 package gameObjects;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import main.GameObject;
+import resources.Sprite;
 
 public class Building extends GameObject {
 	
-	Door door;
+	private String type;
 	
-	public Building () {
+	private Door door;
+	private int doorXOffset;
+	private int doorYOffset;
+	
+	public Building (String type) {
+		
+		this.type = type;
+		Sprite buildingSpr = new Sprite ("resources/sprites/buildings/" + type + "/building.png");
+		setSprite (buildingSpr);
 		
 	}
 	
 	@Override
 	public void onDeclare () {
+		/*File doorOffset = new File ("resources/sprites/buildings/" + type + "/offset.txt");
+		Scanner s;
+		int doorXOffset = 0, doorYOffset = 0;
+		try {
+			s = new Scanner (doorOffset);
+			doorXOffset = s.nextInt ();
+			doorYOffset = s.nextInt ();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		door = new Door ();
+		//door.setSprite (new Sprite ("resources/sprites/buildings/" + type + "/door.png"));
 		door.declare (getX () + 22, getY () + 64);
 	}
 
